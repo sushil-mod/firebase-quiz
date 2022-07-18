ee1388fc3da41e10143c831fe5605573a5629c0c
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2022-07-18 15:02:57 +0530

    Initial setup

---
+++ src/firebase-config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB1MDSL73kmXVfzhgNrpLAO8oE8r4LSxhU",
+    authDomain: "firbase-quiz.firebaseapp.com",
+    projectId: "firbase-quiz",
+    storageBucket: "firbase-quiz.appspot.com",
+    messagingSenderId: "700366518030",
+    appId: "1:700366518030:web:72959909d278a4e677895f",
+    measurementId: "G-8KWEVLBRW6"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
