875f0604a525fa151bafb191f69311945395b6a6
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2022-07-19 23:38:38 +0530

    Feat : Added firebase config

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -11,5 +11,5 @@ const firebaseConfig = {
     measurementId: "G-8KWEVLBRW6"
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
